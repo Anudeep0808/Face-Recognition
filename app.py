23d72db3a517774ae9cf9c4ef5441372f79a09a2
Anudeep Yelakala <96776861+Anudeep0808@users.noreply.github.com>
2025-06-22 20:54:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ EMBEDDING_DIM = 512
 THRESHOLD = 0.3  
 
 app = FaceAnalysis(name='buffalo_l')
-app.prepare(ctx_id=0, det_size=(640, 640))
+app.prepare(ctx_id=-1, det_size=(640, 640))  
 
 def get_face_embedding(image, source=""):
     faces = app.get(image)
